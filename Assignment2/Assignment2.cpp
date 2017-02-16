9b0e26410638799f6cbf882ce9bfec82217b510e
John Parry <ogparry1@ufl.edu>
2017-02-15 21:20:54 -0500

    finished merge sort

--- Assignment2/Assignment2.cpp
+++ Assignment2/Assignment2.cpp
@@ -120,66 +120,74 @@ int min(int k, int l)
 	return ((k < l)?k:l);
 }
 
-int* mergeSort(int* arrToSort, int N)
+void mergeSort(int* keys, int N)
 {
-	int* keys = new int[N];
 	if (N == 2)
 	{
-		keys[0] = max(arrToSort[0], arrToSort[1]);
-		keys[1] = min(arrToSort[0], arrToSort[1]);
+		int temp = min(keys[0], keys[1]);
+		keys[0] = max(keys[0], keys[1]);
+		keys[1] = temp;
 	}
 	else if (N == 3)
 	{
-		int a = arrToSort[0], b = arrToSort[1], c = arrToSort[2];
+		int a = keys[0], b = keys[1], c = keys[2];
 		keys[0] = max(max(a,b),c);
-		if (a == keys[0])
-		{
-			keys[1] = max(b,c);
-			keys[2] = min(b,c);
-		}
-		else if (b == keys[0])
-		{
-			keys[1] = max(a,c);
-			keys[2] = min(a,c);
-		}
-		else
-		{
-			keys[1] = max(a,b);
-			keys[2] = min(a,b);
-		}
+		keys[2] = min(min(a,b),c);
+		keys[1] = (a > keys[2] && a < keys[0]?a:(b > keys[2] && b < keys[0]?b:c));
 	}
 	else
 	{
-		int i = 0, j = 0, k = 0;
+		int i = 0, j = 0;
 		int lastOdd = 0;
 		int* fHalf = new int[N/2]; 
 		int* sHalf = new int[N/2];
-		if (N % 2 != 0) //if N is odd, point to last element and insert it last
-		{
-			lastOdd = arrToSort[N-1];
-		}
+		if (N % 2 != 0) lastOdd = keys[N-1]; //if N is odd, point to last element and insert it last
 		for (int ii = 0; ii < N/2; ii++) //fill in halves
 		{
-			fHalf[ii] = arrToSort[ii];
-			sHalf[ii] = arrToSort[N/2+ii+1];
+			fHalf[ii] = keys[ii];
+			sHalf[ii] = keys[N/2+ii+1];
 		}
+		mergeSort(fHalf, N/2);
+		mergeSort(sHalf, N/2);
 		for (int ii = 0; ii < N; ii++) //merge halves
 		{
+			if (lastOdd > fHalf[i] && lastOdd > sHalf[i] && lastOdd != 0)
+			{
+				keys[ii] = lastOdd;
+				lastOdd = 0;
+			}
 			if (i != N/2 && j != N/2)
 			{
-				//keys[k] = max(max(fHalf[i]
+				if (fHalf[i] >= sHalf[j])
+				{
+					keys[ii] = fHalf[i];
+					i++;
+				}
+				else
+				{
+					keys[ii] = sHalf[j];
+					j++;
+				}
 			}
 			else if (i == N/2)
 			{
-			
+				for (int kk = ii; kk < N; kk++) //fill in halves
+				{
+					keys[ii] = sHalf[j];
+					j++;
+				}
+				break;
 			}
 			else if (j == N/2)
 			{
-			
+				for (int kk = ii; kk < N; kk++) //fill in halves
+				{
+					keys[ii] = fHalf[i];
+					i++;
+				}
+				break;
 			}
-			k++;
 		}
-	return keys;
 	}
 }
 
