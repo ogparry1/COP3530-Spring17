0ed5608eb5e80e34cbc0441a0b0985e741641796
John Parry <ogparry1@ufl.edu>
2017-02-15 21:25:37 -0500

    a little rearranging

--- Assignment2/Assignment2.cpp
+++ Assignment2/Assignment2.cpp
@@ -3,6 +3,87 @@
 
 using namespace std;
 
+int max(int k, int l)
+{
+	return ((k > l)?k:l);
+}
+
+int min(int k, int l)
+{
+	return ((k < l)?k:l);
+}
+
+void mergeSort(int* keys, int N)
+{
+	if (N == 2)
+	{
+		int temp = min(keys[0], keys[1]);
+		keys[0] = max(keys[0], keys[1]);
+		keys[1] = temp;
+	}
+	else if (N == 3)
+	{
+		int a = keys[0], b = keys[1], c = keys[2];
+		keys[0] = max(max(a,b),c);
+		keys[2] = min(min(a,b),c);
+		keys[1] = (a > keys[2] && a < keys[0]?a:(b > keys[2] && b < keys[0]?b:c));
+	}
+	else
+	{
+		int i = 0, j = 0;
+		int lastOdd = 0;
+		int* fHalf = new int[N/2]; 
+		int* sHalf = new int[N/2];
+		if (N % 2 != 0) lastOdd = keys[N-1]; //if N is odd, point to last element and insert it last
+		for (int ii = 0; ii < N/2; ii++) //fill in halves
+		{
+			fHalf[ii] = keys[ii];
+			sHalf[ii] = keys[N/2+ii+1];
+		}
+		mergeSort(fHalf, N/2);
+		mergeSort(sHalf, N/2);
+		for (int ii = 0; ii < N; ii++) //merge halves
+		{
+			if (lastOdd > fHalf[i] && lastOdd > sHalf[i] && lastOdd != 0)
+			{
+				keys[ii] = lastOdd;
+				lastOdd = 0;
+			}
+			if (i != N/2 && j != N/2)
+			{
+				if (fHalf[i] >= sHalf[j])
+				{
+					keys[ii] = fHalf[i];
+					i++;
+				}
+				else
+				{
+					keys[ii] = sHalf[j];
+					j++;
+				}
+			}
+			else if (i == N/2)
+			{
+				for (int kk = ii; kk < N; kk++) //fill in halves
+				{
+					keys[ii] = sHalf[j];
+					j++;
+				}
+				break;
+			}
+			else if (j == N/2)
+			{
+				for (int kk = ii; kk < N; kk++) //fill in halves
+				{
+					keys[ii] = fHalf[i];
+					i++;
+				}
+				break;
+			}
+		}
+	}
+}
+
 class Node								//Good To Go
 {
 	public:
@@ -110,87 +191,6 @@ HashTable::~HashTable()					//Good To Go*
 	}
 }
 
-int max(int k, int l)
-{
-	return ((k > l)?k:l);
-}
-
-int min(int k, int l)
-{
-	return ((k < l)?k:l);
-}
-
-void mergeSort(int* keys, int N)
-{
-	if (N == 2)
-	{
-		int temp = min(keys[0], keys[1]);
-		keys[0] = max(keys[0], keys[1]);
-		keys[1] = temp;
-	}
-	else if (N == 3)
-	{
-		int a = keys[0], b = keys[1], c = keys[2];
-		keys[0] = max(max(a,b),c);
-		keys[2] = min(min(a,b),c);
-		keys[1] = (a > keys[2] && a < keys[0]?a:(b > keys[2] && b < keys[0]?b:c));
-	}
-	else
-	{
-		int i = 0, j = 0;
-		int lastOdd = 0;
-		int* fHalf = new int[N/2]; 
-		int* sHalf = new int[N/2];
-		if (N % 2 != 0) lastOdd = keys[N-1]; //if N is odd, point to last element and insert it last
-		for (int ii = 0; ii < N/2; ii++) //fill in halves
-		{
-			fHalf[ii] = keys[ii];
-			sHalf[ii] = keys[N/2+ii+1];
-		}
-		mergeSort(fHalf, N/2);
-		mergeSort(sHalf, N/2);
-		for (int ii = 0; ii < N; ii++) //merge halves
-		{
-			if (lastOdd > fHalf[i] && lastOdd > sHalf[i] && lastOdd != 0)
-			{
-				keys[ii] = lastOdd;
-				lastOdd = 0;
-			}
-			if (i != N/2 && j != N/2)
-			{
-				if (fHalf[i] >= sHalf[j])
-				{
-					keys[ii] = fHalf[i];
-					i++;
-				}
-				else
-				{
-					keys[ii] = sHalf[j];
-					j++;
-				}
-			}
-			else if (i == N/2)
-			{
-				for (int kk = ii; kk < N; kk++) //fill in halves
-				{
-					keys[ii] = sHalf[j];
-					j++;
-				}
-				break;
-			}
-			else if (j == N/2)
-			{
-				for (int kk = ii; kk < N; kk++) //fill in halves
-				{
-					keys[ii] = fHalf[i];
-					i++;
-				}
-				break;
-			}
-		}
-	}
-}
-
 int main()								//Needs Work and to check input
 {
 	int N = 0; // maxProduct = -1;
@@ -204,9 +204,7 @@ int main()								//Needs Work and to check input
 		factors.insert(k); //factor a number into its product pairs and insert them in hash table
 		keys[ii] = k;
 	}
-	//sort keys in descending order
-	int* KEYS = keys;
-	mergeSort(KEYS, N);
+	mergeSort(keys, N); //sort keys in descending order
 	//retreive hashed factor pairs and try them against the lesser inputs than their product
 	//return the first key to pass test or return -1
 	//check for a,b,c and return c or -1
