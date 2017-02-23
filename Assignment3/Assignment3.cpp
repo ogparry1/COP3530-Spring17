d8162c8ab921c2da3027e6297803c33212d864cb
John Parry <ogparry1@ufl.edu>
2017-02-23 02:49:14 -0500

    nearly done with problem 3, just a few changes need to be made to reconstruct()

--- Assignment3/Assignment3.cpp
+++ Assignment3/Assignment3.cpp
@@ -114,27 +114,75 @@ class BinTree
 	public:
 		BinTree(int);
 		BinNode* root;
-		
+		void construct();
+		BinNode* reconstruct(int Apost[], int Bin[]);
 	private:
 		BinNode** init;
+		int size;
 };
 
 BinTree::BinTree(int N)
 {
-	int value = 0, lc = 0, rc = 0;
 	init = new BinNode*[N];
 	for (int ii = 0; ii < N; ii++)
 	{
 		init[ii] = new BinNode(ii);
 	}
-	for (int ii = 0; ii < N; ii++)
+	root = init[0];
+	size = N;
+}
+
+void BinTree::construct()
+{
+	int value = 0, lc = 0, rc = 0;
+	for (int ii = 0; ii < size; ii++)
 	{
 		cin >> value >> lc >> rc;
 		init[ii]->setValue(value);
 		init[ii]->LeftChild = (lc != -1?init[lc]:NULL);
 		init[ii]->RightChild = (rc != -1?init[rc]:NULL);
 	}
-	root = init[0];
+}
+
+BinNode* BinTree::reconstruct(int Apost[], int Bin[])
+{
+	BinNode* node = NULL; BinNode* hold = NULL;
+	int i = 0, j = 1, n = 0;
+	while (i < size)
+	{
+		if (i == 0)
+		{
+			node = new BinNode(-1);
+			node->setValue(Bin[i]);
+			i++;
+		}
+		else
+		{
+			hold = node;
+			node = new BinNode(-1);
+			node->setValue(Bin[i]);
+			node->LeftChild = hold;
+			while (Apost[j] != Bin[i])
+			{
+				j++;
+				n++;
+			}
+			int pos[n];
+			int in[n];
+			for (int ii = j-n; ii < j; ii++)
+			{
+				pos[ii] = Apost[ii];
+			}
+			for (int ii = i+1; ii <= i+n; ii++)
+			{
+				in[ii] = Bin[ii];
+			}
+			node->RightChild = reconstruct(pos, in);
+			j++;
+			i = j;
+		}
+	}
+	return node;
 }
 
 int main()
@@ -143,6 +191,7 @@ int main()
 	cin >> N;  //input number of Nodes
 	BinTree tree(N);
 	/* Problem 1 P/I/P order traversal
+	 tree.construct();
 	 tree.root->preorder();
 	 cout << endl;
 	 tree.root->inorder();
@@ -151,7 +200,7 @@ int main()
 	 * */
 	 
 	 /* Problem 2 Level order traversal
-	 * */
+	 tree.construct();
 	 Queue queue;
 	 BinNode* temp = tree.root;
 	 while (temp != NULL)
@@ -167,10 +216,35 @@ int main()
 		 cout << temp->getValue() << " ";
 		 temp = queue.pop();
 	 }
-	 
+	 * */
 	 
 	 /* Problem 3
 	 * */
+	 int Apost[N]; int Bin[N];
+	 for (int ii = 0; ii < N; ii++)
+	 {
+		 cin >> Apost[ii];
+	 }
+	 for (int ii = 0; ii < N; ii++)
+	 {
+		 cin >> Bin[ii];
+	 }
+	 tree.root = tree.reconstruct(Apost, Bin);
+	 Queue queue;
+	 while (tree.root != NULL)
+	 {
+		 if (tree.root->LeftChild != NULL)
+		 {
+			queue.push(tree.root->LeftChild);
+		 }
+		 if (tree.root->RightChild != NULL)
+		 {
+			queue.push(tree.root->RightChild);
+		 }
+		 cout << tree.root->getValue() << " ";
+		 tree.root = queue.pop();
+	 }
+	 
 	return 0;
 }
 
