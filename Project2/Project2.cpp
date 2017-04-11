2905970883e53947882606d37db0e6235b35ac9e
John Parry <ogparry1@ufl.edu>
2017-04-11 12:16:14 -0400

    needed to compare forward and backward iterations so more editing is needed

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -5,47 +5,66 @@ using namespace std;
 
 int main()
 {
-	string key = "", rkey = "", hint = "";
-	string icode = "", rcode = "";
+	string key = "", hint = "", rkey = "", rhint = "";
+	string ficode = "", frcode = "", finalcode = "";
 	getline(cin, key);
 	getline(cin, hint);
+	int icode[key.length()], rcode[key.length()];
+	int iIndex = -1, rIndex = -1;
 	int code[4] = {-1, -1, -1, -1};
 	cout << "";
 	for (int ii = key.length()-1; ii >= 0; ii--) 
 	{
+		if (ii <= (int)hint.length()-1) rhint += hint[ii];
 		rkey += key[ii];
 	}
+	bool statement = false;
 	for (int ii = 0; ii <= (int)(key.length()-hint.length()); ii++)
 	{
-		if (key.substr(ii, hint.length()) == hint)
+		if (key.substr(ii, hint.length()) == hint) //finds hint across forward code
 		{
 			code[1] = ii;
-			if (ii != 0)
+			if (code[1] >= 0 && code[0] >= 0) 
 			{
-				if (code[0] >= 0) 
-				{
-					int diff = code[1] - code[0];
-					icode = icode + to_string(diff) + " ";
-				}
-				code[0] = code[1];
+				int diff = code[1] - code[0];
+				icode[iIndex++] = diff;
 			}
-			else code[0] = code[1];
+			code[0] = code[1];
+			statement = true;
 		}
-		if (rkey.substr(ii, hint.length()) == hint)
+		if (rkey.substr(ii, rhint.length()) == rhint) //finds hint across reverse code
 		{
 			code[3] = ii;
-			if (ii != 0)
+			if (code[3] >= 0 && code[2] >= 0) 
 			{
-				if (code[2] >= 0) 
-				{
-					int diff = code[3] - code[2];
-					rcode = rcode + to_string(diff) + " ";
-				}
-				code[2] = code[3];
+				int diff = code[3] - code[2];
+				rcode[rIndex++] = diff;
 			}
-			else code[2] = code[3];
+			code[2] = code[3];
+			statement = true
+		}
+		if (iIndex >= 0 && rIndex >= 0 && statement)
+		{
+			if ()// creates longest finalcode comparing icode to rcode
+			{
+				
+			}
+			if ()// creates longest finalcode comparing rcode to icode
+			{
+				
+			}
+		}
+		
+	}
+	string finalcode = "";
+	for (int ii = 0; ii < (int)icode.length(); ii++)
+	{
+		for (int jj = 0; jj < (int)icode.length(); jj++)
+		{
+			if () finalcode += rcode[jj] ;
 		}
 	}
+	cout << (icode.length() <= rcode.length() ? icode : rcode) << endl;
 	cout << (icode.length() > rcode.length() ? icode : rcode);
 	return 0;
 }
