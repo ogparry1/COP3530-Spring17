44982b66ea824009b3ec08dc93324c92aa694ebb
John Parry <ogparry1@ufl.edu>
2017-04-11 17:33:07 -0400

    Project2 finished

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -6,16 +6,13 @@ using namespace std;
 int main()
 {
 	string key = "", hint = "";	// key and hint
-	string ficode = "";	// derived forward and reverse order codes
+	string ficode = "", frcode = "";	// derived forward and reverse order codes
 	getline(cin, key);	// get key
 	getline(cin, hint);	// get hint
 	int icode[key.length()]; // define arrays for forward and reverse indexes
-	int icodeIndex = -1, iHold = 0; // index in icode, traversal index
-	int next = 0, first = 0; // indexes to compare
+	int icodeIndex = -1, next = -1, first = -1, posi = 0, posr = 0; // index in icode, traversal index, indexes to compare
 	cout << "";	// default output of empty line
-	bool event = false;		// states if pattern was found
-	cout << "key = " << key << " and hint = " << hint << endl;
-	for (int ii = 0; ii <= (int)(key.length()-hint.length()); ii++)
+	for (int ii = 0; ii <= (int)(key.length() - hint.length()); ii++)
 	{
 		if (key.substr(ii, hint.length()) == hint) // finds hint across forward key
 		{
@@ -26,40 +23,32 @@ int main()
 				icode[++icodeIndex] = diff;
 			}
 			first = next;
-			event = true;
-		}
-		if (icodeIndex >= 0 && rcodeIndex >= 0 && event) // creates longest final code comparing icode to rcode
-		{
-			if (key.substr(ii, hint.length()) == hint) // if event occurs in icode
-			{
-				for (int jj = rHold; jj < rcodeIndex+1; jj++) // search rcode for new icode insert
-				{
-					if (icode[icodeIndex] == rcode[jj])
-					{
-						ficode = ficode + to_string(icode[icodeIndex]) + " ";
-						rHold = jj + 1;
-						break;
-					}
-				}
-			}
-			if (rkey.substr(ii, rhint.length()) == rhint)// if event occurs in rcode
-			{
-				for (int jj = iHold; jj < icodeIndex+1; jj++) // search icode for new rcode insert
-				{
-					if (rcode[rcodeIndex] == icode[jj])
-					{
-						frcode = frcode + to_string(rcode[rcodeIndex]) + " ";
-						iHold = jj + 1;
-						break;
-					}
-				}
-			}
-			event = false;
 		}
 	}
-	cout << ficode << endl << frcode << endl;
-	//cout << (ficode.length() <= frcode.length() ? ficode : frcode) << endl;
-	//cout << (ficode.length() > frcode.length() ? ficode : frcode);
+	int rcode[icodeIndex+1];
+	for (int ii = 0; ii <= icodeIndex; ii++) { rcode[ii] = icode[icodeIndex-ii]; }
+	for (int ii = 0; ii <= icodeIndex; ii++)
+	{
+		for (int jj = posi; jj <= icodeIndex; jj++)
+		{
+			if (icode[ii] == rcode[jj])
+			{
+				ficode += to_string(rcode[jj]) + " ";
+				posi = jj+1;
+				break;
+			}
+		}
+		for (int jj = posr; jj <= icodeIndex; jj++)
+		{
+			if (rcode[ii] == icode[jj])
+			{
+				frcode += to_string(icode[jj]) + " ";
+				posr = jj+1;
+				break;
+			}
+		}
+	}
+	cout << (ficode.length() > frcode.length() ? ficode : frcode);
 	return 0;
 }
 
