c45ce01c2cad54a3341d7583aa920c669eefab11
John Parry <ogparry1@ufl.edu>
2017-04-11 13:04:50 -0400

    nadded functionality to compare icode and rcode. still has bugs however

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -5,67 +5,76 @@ using namespace std;
 
 int main()
 {
-	string key = "", hint = "", rkey = "", rhint = "";
-	string ficode = "", frcode = "", finalcode = "";
-	getline(cin, key);
-	getline(cin, hint);
-	int icode[key.length()], rcode[key.length()];
-	int iIndex = -1, rIndex = -1;
-	int code[4] = {-1, -1, -1, -1};
-	cout << "";
-	for (int ii = key.length()-1; ii >= 0; ii--) 
+	string key = "", hint = "", rkey = "", rhint = "";	// keys and hints
+	string ficode = "", frcode = "";	// derived forward and reverse order codes
+	getline(cin, key);	// get key
+	getline(cin, hint);	// get hint
+	int icode[key.length()], rcode[key.length()];	// define arrays for forward and reverse indexes
+	int icodeIndex = -1, rcodeIndex = -1;	// index in icode and rcode
+	int iHold = 0, rHold = 0; // traversal indexes
+	int code[4] = {-1, -1, -1, -1};		// indexes to compare
+	cout << "";		// default output of empty line
+	for (int ii = key.length()-1; ii >= 0; ii--) // build rkey and rhint
 	{
 		if (ii <= (int)hint.length()-1) rhint += hint[ii];
 		rkey += key[ii];
 	}
-	bool statement = false;
+	bool event = false;		// states if pattern was found
 	for (int ii = 0; ii <= (int)(key.length()-hint.length()); ii++)
 	{
-		if (key.substr(ii, hint.length()) == hint) //finds hint across forward code
+		if (key.substr(ii, hint.length()) == hint) // finds hint across forward key
 		{
 			code[1] = ii;
 			if (code[1] >= 0 && code[0] >= 0) 
 			{
 				int diff = code[1] - code[0];
-				icode[iIndex++] = diff;
+				icode[++icodeIndex] = diff;
 			}
 			code[0] = code[1];
-			statement = true;
+			event = true;
 		}
-		if (rkey.substr(ii, rhint.length()) == rhint) //finds hint across reverse code
+		if (rkey.substr(ii, rhint.length()) == rhint) // finds hint across reverse key
 		{
 			code[3] = ii;
 			if (code[3] >= 0 && code[2] >= 0) 
 			{
 				int diff = code[3] - code[2];
-				rcode[rIndex++] = diff;
+				rcode[++rcodeIndex] = diff;
 			}
 			code[2] = code[3];
-			statement = true
+			event = true;
 		}
-		if (iIndex >= 0 && rIndex >= 0 && statement)
+		if (icodeIndex >= 0 && rcodeIndex >= 0 && event) // creates longest final code comparing icode to rcode
 		{
-			if ()// creates longest finalcode comparing icode to rcode
+			if (key.substr(ii, hint.length()) == hint) // if event occurs in icode
 			{
-				
+				for (int jj = rHold; jj < rcodeIndex+1; jj++) // search rcode for new icode insert
+				{
+					if (icode[icodeIndex] == rcode[jj])
+					{
+						ficode = ficode + to_string(icode[icodeIndex]) + " ";
+						rHold = jj + 1;
+						break;
+					}
+				}
 			}
-			if ()// creates longest finalcode comparing rcode to icode
+			if (rkey.substr(ii, rhint.length()) == rhint)// if event occurs in rcode
 			{
-				
+				for (int jj = iHold; jj < icodeIndex+1; jj++) // search rcode for new icode insert
+				{
+					if (rcode[rcodeIndex] == icode[jj])
+					{
+						frcode = frcode + to_string(rcode[rcodeIndex]) + " ";
+						iHold = jj + 1;
+						break;
+					}
+				}
 			}
-		}
-		
-	}
-	string finalcode = "";
-	for (int ii = 0; ii < (int)icode.length(); ii++)
-	{
-		for (int jj = 0; jj < (int)icode.length(); jj++)
-		{
-			if () finalcode += rcode[jj] ;
+			event = false;
 		}
 	}
-	cout << (icode.length() <= rcode.length() ? icode : rcode) << endl;
-	cout << (icode.length() > rcode.length() ? icode : rcode);
+	cout << (ficode.length() <= frcode.length() ? ficode : frcode) << endl;
+	cout << (ficode.length() > frcode.length() ? ficode : frcode);
 	return 0;
 }
 
