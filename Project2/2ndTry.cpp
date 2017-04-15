032c6299b58a8f99ee110df6247f4711c75b2702
John Parry <ogparry1@ufl.edu>
2017-04-15 02:28:31 -0400

    Still trying to get a final print, but its not working out. Using structs now though and the recursion in mostly programmed in

--- Project2/2ndTry.cpp
+++ Project2/2ndTry.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
+struct Passcode
+{
+	string code;
+	int length;
+};
 
+Passcode* pswd(int* fwd, int* bkwd, int findex, int bindex, Passcode* pass)
+{
+	if (fwd[findex] == bkwd[bindex])
+	{
+		pass->code = to_string(fwd[findex]) + " " + pass->code;
+		if (findex != 0 && bindex != 0) pass->length = pswd(fwd, bkwd, findex-1, bindex-1, pass)->length;
+		else pass->length = 1;
+		return pass;
+	}
+	else
+	{
+		if (findex != 0 && bindex != 0)
+		{
+			Passcode pass1 = *pass, pass2 = *pass;
+			pass1.length = pswd(fwd, bkwd, findex-1, bindex, &pass1)->length;
+			pass2.length = pswd(fwd, bkwd, findex, bindex-1, &pass2)->length;
+			pass->code = (pass1.length > pass2.length ? pass1.code : pass2.code) + " " + pass->code;
+			pass->length += (pass1.length > pass2.length ? pass1.length : pass2.length);
+			return pass;
+		}
+		else return 0;
+	}
+}
+
+string password(int* code, int index)
+{
+	int *diff = new int[index+1], *ffid = new int[index+1];
+	Passcode* pass;
+	for (int ii = 0; ii <= index; ii++) 
+	{
+		diff[ii] = code[ii];
+		ffid[ii] = code[index-ii];
+	}
+	pswd(diff, ffid, index, index, pass);
+	return pass->code;
+}
 
 char* StoC(string str)
 {
@@ -53,30 +95,24 @@ int main()
 	int ki = 0, hi = 0, di = 0, temp = -1;
 	while (ki < keylength)
 	{
-		if (hi == hintlength)
-		{
-			if (temp != -1)
-			{
-				diff[di] = (ki-hi) - temp;
-				di++;
-			}
-			temp = ki - hi;
-		}
-		
 		if (key[ki] == hint[hi])
 		{
 			ki++;
 			hi++;
 		}
-		else
+		if (hi == hintlength)
+		{
+			if (temp != -1) diff[di++] = (ki-hi) - temp;
+			temp = ki - hi;
+			hi = pattern[hi-1];
+		}
+		else if (ki < keylength && key[ki] != hint[hi])
 		{
 			if (hi != 0) hi = pattern[hi-1];
-			else
-			{
-				ki++;
-			}
+			else ki++;
 		}
 	}
+	cout << password(diff, di-1);
 	
 	return 0;
 }
