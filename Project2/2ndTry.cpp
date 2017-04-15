59bc95cfd9c86a7bdb7c753532fb746208e8bef6
John Parry <ogparry1@ufl.edu>
2017-04-14 23:06:41 -0400

    showPattern() is finished and the search algorithm is nearly finished

--- Project2/2ndTry.cpp
+++ Project2/2ndTry.cpp
@@ -13,11 +13,28 @@ char* StoC(string str)
 
 int* showPattern(char* hint, int length)
 {
-	int pattern[length];
-	int ii = 1;
+	int* pattern = new int[length];
+	int ii = 1, last = 0;
 	while (ii < length)
 	{
-		if ()
+		if (hint[ii] == hint[last])
+		{
+			last++;
+			pattern[ii] = last;
+			ii++;
+		}
+		else
+		{
+			if (last != 0)
+			{
+				last = pattern[last-1];
+			}
+			else
+			{
+				pattern[ii] = 0;
+				ii++;
+			}
+		}
 	}
 	return pattern;
 }
@@ -32,7 +49,34 @@ int main()
 	int hintlength = tmp.length();
 	char* hint = StoC(tmp);
 	int* pattern = showPattern(hint, hintlength);  // O(W)
-	
+	int* diff = new int[keylength];
+	int ki = 0, hi = 0, di = 0, temp = -1;
+	while (ki < keylength)
+	{
+		if (hi == hintlength)
+		{
+			if (temp != -1)
+			{
+				diff[di] = (ki-hi) - temp;
+				di++;
+			}
+			temp = ki - hi;
+		}
+		
+		if (key[ki] == hint[hi])
+		{
+			ki++;
+			hi++;
+		}
+		else
+		{
+			if (hi != 0) hi = pattern[hi-1];
+			else
+			{
+				ki++;
+			}
+		}
+	}
 	
 	return 0;
 }
