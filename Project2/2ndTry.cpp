3f08b4a40a4c44765a6b4f4168a92748fa384e15
John Parry <ogparry1@ufl.edu>
2017-04-16 12:42:13 -0400

    refactoring complete, no output at the moment

--- Project2/2ndTry.cpp
+++ Project2/2ndTry.cpp
@@ -37,7 +37,9 @@ class Password  // class that holds the all provided info
 {
 	public:
 		Password(string, string);
+		Password(Password, int, int);
 		void resize();
+		void makediff();
 		string key, hint, code;
 		int *diff, *ffid, *pattern;
 		int klen, hlen, codelen, dii, fii;
@@ -47,53 +49,75 @@ Password::Password(string key, string hint)
 {
 	this->key = key; this->hint = hint;
 	code = "";
-	klen = key.length; hlen = hint.length;
+	klen = key.length(); hlen = hint.length();
 	pattern = showPattern(hint.c_str(), hlen);  // O(W)
-	int *diff = new int[klen], *ffid = new int[klen];
+	diff = new int[klen], ffid = new int[klen];
 	dii = 0; fii = 0; codelen = 0;
+	makediff();
 }
 
-void Password::resize()
+void Password::resize() //resizes the arrays of diff and ffid to fit their data
 {
-	int tmp1[dii], tmp2[dii]
+	int tmp1[dii], tmp2[dii];
 	for (int ii = 0; ii < dii; ii++) 
 	{
 		tmp1[ii] = diff[ii];
-		tmp2[ii] = ffid[index-ii];
+		tmp2[ii] = ffid[dii-ii];
 	}
 	diff = tmp1; ffid = tmp2;
 }
 
-Password diffid(Password pass)
+void Password::makediff()
 {
-	if (pass.diff[pass.dii] == ffid[pass.fii])
+	int ki = 0, hi = 0, temp = -1;
+	while (ki < klen)
+	{
+		if (key[ki] == hint[hi])
+		{
+			ki++;
+			hi++;
+		}
+		if (hi == hlen)
+		{
+			if (temp != -1) diff[dii++] = (ki-hi) - temp;
+			temp = ki - hi;
+			hi = pattern[hi-1];
+		}
+		else if (ki < klen && key[ki] != hint[hi])
+		{
+			if (hi != 0) hi = pattern[hi-1];
+			else ki++;
+		}
+	}
+	resize();
+}
+
+Password diffid(Password pass) // compares diff and ffid
+{
+	if (pass.diff[pass.dii] == pass.ffid[pass.fii])
 	{
 		pass.code = to_string(pass.diff[pass.dii]) + " " + pass.code;
 		cout << pass.code << endl;
-		if (pass.dii != 0 && pass.fii != 0) pass.codelen = diffid.codelen;
-		else pass.length = 1;
+		if (pass.dii != 0 && pass.fii != 0) 
+		{
+			pass.dii--; pass.fii--;
+			pass.codelen += 1 + diffid(pass).codelen;
+		}
+		else pass.codelen += 1;
 		return pass;
 	}
 	else
 	{
 		if (pass.dii != 0 && pass.fii != 0)
 		{
-			Password *pass1 = new Password{pass.code, pass.length}, *pass2 = new Password{pass.code, pass.length};
-			pass1.length = pswd(fwd, bkwd, pass.dii-1, pass.fii, pass1).length;
-			pass2.length = pswd(fwd, bkwd, pass.dii, pass.fii-1, pass2).length;
-			pass.code = (pass1.length > pass2.length ? pass1.code : pass2.code) + " " + pass.code;
-			pass.length += (pass1.length > pass2.length ? pass1.length : pass2.length);
-			return pass;
+			Password pass1 = Password(pass), pass2 = Password(pass);
+			pass1.dii--; pass2.fii--;
+			pass1 = diffid(pass1); pass2 = diffid(pass2);
+			pass.code = (pass1.codelen > pass2.codelen ? pass1.code : pass2.code) + " " + pass.code;
+			pass.codelen += (pass1.codelen > pass2.codelen ? pass1.codelen : pass2.codelen);
 		}
-		else return 0;
+		return pass;
 	}
-}
-
-string passcode(Password pass)
-{
-	pass.resize();
-	
-	return pass.code;
 }
 
 int main()
@@ -101,28 +125,8 @@ int main()
 	string key = "", hint = "";
 	getline(cin, key);
 	getline(cin, hint);
-	Password code = Password(key, hint);
-	int ki = 0, hi = 0, di = 0, temp = -1;
-	while (ki < key.length)
-	{
-		if (key.code[ki] == hint.code[hi])
-		{
-			ki++;
-			hi++;
-		}
-		if (hi == hint.length)
-		{
-			if (temp != -1) diff[di++] = (ki-hi) - temp;
-			temp = ki - hi;
-			hi = pattern[hi-1];
-		}
-		else if (ki < key.length && key.code[ki] != hint.code[hi])
-		{
-			if (hi != 0) hi = pattern[hi-1];
-			else ki++;
-		}
-	}
-	cout << password(diff, di-1);
-	
+	Password result = Password(key, hint);
+	diffid(result);
+	cout << result.code;
 	return 0;
 }
