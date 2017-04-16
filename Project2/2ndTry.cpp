fab12f76f792ce00fdd0826960faad7a22174462
John Parry <ogparry1@ufl.edu>
2017-04-16 11:55:10 -0400

    refactoring with classes

--- Project2/2ndTry.cpp
+++ Project2/2ndTry.cpp
@@ -1,59 +1,11 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <cstdlib>
 
 using namespace std;
 
-struct Passcode
-{
-	string code;
-	int length;
-};
-
-Passcode* pswd(int* fwd, int* bkwd, int findex, int bindex, Passcode* pass)
-{
-	if (fwd[findex] == bkwd[bindex])
-	{
-		pass->code = to_string(fwd[findex]) + " " + pass->code;
-		if (findex != 0 && bindex != 0) pass->length = pswd(fwd, bkwd, findex-1, bindex-1, pass)->length;
-		else pass->length = 1;
-		return pass;
-	}
-	else
-	{
-		if (findex != 0 && bindex != 0)
-		{
-			Passcode pass1 = *pass, pass2 = *pass;
-			pass1.length = pswd(fwd, bkwd, findex-1, bindex, &pass1)->length;
-			pass2.length = pswd(fwd, bkwd, findex, bindex-1, &pass2)->length;
-			pass->code = (pass1.length > pass2.length ? pass1.code : pass2.code) + " " + pass->code;
-			pass->length += (pass1.length > pass2.length ? pass1.length : pass2.length);
-			return pass;
-		}
-		else return 0;
-	}
-}
-
-string password(int* code, int index)
-{
-	int *diff = new int[index+1], *ffid = new int[index+1];
-	Passcode* pass;
-	for (int ii = 0; ii <= index; ii++) 
-	{
-		diff[ii] = code[ii];
-		ffid[ii] = code[index-ii];
-	}
-	pswd(diff, ffid, index, index, pass);
-	return pass->code;
-}
-
-char* StoC(string str)
-{
-	const char* convert = str.c_str();
-	return (char*)convert;
-}
-
-int* showPattern(char* hint, int length)
+int* showPattern(const char* hint, int length)  //shows the pattern of hint
 {
 	int* pattern = new int[length];
 	int ii = 1, last = 0;
@@ -81,32 +33,90 @@ int* showPattern(char* hint, int length)
 	return pattern;
 }
 
+class Password  // class that holds the all provided info
+{
+	public:
+		Password(string, string);
+		void resize();
+		string key, hint, code;
+		int *diff, *ffid, *pattern;
+		int klen, hlen, codelen, dii, fii;
+};
+
+Password::Password(string key, string hint)
+{
+	this->key = key; this->hint = hint;
+	code = "";
+	klen = key.length; hlen = hint.length;
+	pattern = showPattern(hint.c_str(), hlen);  // O(W)
+	int *diff = new int[klen], *ffid = new int[klen];
+	dii = 0; fii = 0; codelen = 0;
+}
+
+void Password::resize()
+{
+	int tmp1[dii], tmp2[dii]
+	for (int ii = 0; ii < dii; ii++) 
+	{
+		tmp1[ii] = diff[ii];
+		tmp2[ii] = ffid[index-ii];
+	}
+	diff = tmp1; ffid = tmp2;
+}
+
+Password diffid(Password pass)
+{
+	if (pass.diff[pass.dii] == ffid[pass.fii])
+	{
+		pass.code = to_string(pass.diff[pass.dii]) + " " + pass.code;
+		cout << pass.code << endl;
+		if (pass.dii != 0 && pass.fii != 0) pass.codelen = diffid.codelen;
+		else pass.length = 1;
+		return pass;
+	}
+	else
+	{
+		if (pass.dii != 0 && pass.fii != 0)
+		{
+			Password *pass1 = new Password{pass.code, pass.length}, *pass2 = new Password{pass.code, pass.length};
+			pass1.length = pswd(fwd, bkwd, pass.dii-1, pass.fii, pass1).length;
+			pass2.length = pswd(fwd, bkwd, pass.dii, pass.fii-1, pass2).length;
+			pass.code = (pass1.length > pass2.length ? pass1.code : pass2.code) + " " + pass.code;
+			pass.length += (pass1.length > pass2.length ? pass1.length : pass2.length);
+			return pass;
+		}
+		else return 0;
+	}
+}
+
+string passcode(Password pass)
+{
+	pass.resize();
+	
+	return pass.code;
+}
+
 int main()
 {
-	string tmp = "";
-	getline(cin, tmp);
-	int keylength = tmp.length();
-	char* key = StoC(tmp);
-	getline(cin, tmp);
-	int hintlength = tmp.length();
-	char* hint = StoC(tmp);
-	int* pattern = showPattern(hint, hintlength);  // O(W)
-	int* diff = new int[keylength];
+	string key = "", hint = "";
+	getline(cin, key);
+	getline(cin, hint);
+	Password code = Password(key, hint);
 	int ki = 0, hi = 0, di = 0, temp = -1;
-	while (ki < keylength)
+	while (ki < key.length)
 	{
-		if (key[ki] == hint[hi])
+		if (key.code[ki] == hint.code[hi])
 		{
 			ki++;
 			hi++;
 		}
-		if (hi == hintlength)
+		if (hi == hint.length)
 		{
 			if (temp != -1) diff[di++] = (ki-hi) - temp;
 			temp = ki - hi;
 			hi = pattern[hi-1];
 		}
-		else if (ki < keylength && key[ki] != hint[hi])
+		else if (ki < key.length && key.code[ki] != hint.code[hi])
 		{
 			if (hi != 0) hi = pattern[hi-1];
 			else ki++;
