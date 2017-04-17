830bb19ab331b10d334e13d642948430fa225c54
John Parry <ogparry1@ufl.edu>
2017-04-16 21:57:07 -0400

    having some troubles in the diffid method with recursion.  The algorithm is solid but it seems to be returning in reverse order so there's nothing left at the final print

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 void showPattern(const char* hint, int length, int* pattern)  //shows the pattern of hint
 {
+	// from Password constructor
 	int last = 0, ii = 1;
 	pattern[0] = 0;
 	while (ii < length)
@@ -31,6 +32,7 @@ void showPattern(const char* hint, int length, int* pattern)  //shows the patter
 			}
 		}
 	}
+	// to Password constructor
 }
 
 class Password  // class that holds the all provided info
@@ -38,40 +40,42 @@ class Password  // class that holds the all provided info
 	public:
 		Password(string, string);
 		Password(Password, int, int);
-		void resize();
-		void makediff();
+		void reverse(int*);
+		int* makediff();
 		string key, hint, code;
-		int *diff, *ffid, *pattern;
+		int* diff; int* ffid; int* pattern;
 		int klen, hlen, codelen, dii, fii;
 };
 
 Password::Password(string key, string hint)
 {
+	// from main or diffid
 	this->key = key; this->hint = hint;
-	code = "";
+	dii = 0; fii = 0; codelen = 0; code = "";
 	klen = key.length(); hlen = hint.length();
 	pattern = new int[hlen];
 	showPattern(hint.c_str(), hlen, pattern);  // O(W)
-	diff = new int[klen], ffid = new int[klen];
-	dii = 0; fii = 0; codelen = 0;
-	makediff();
+	reverse(makediff());
 }
 
-void Password::resize() //resizes the arrays of diff and ffid to fit their data
+void Password::reverse(int* differences) //resizes the arrays of diff and ffid to fit their data
 {
-	int tmp1[dii], tmp2[dii];
+	// from makediff
+	fii = dii;
+	diff = new int[dii]; ffid = new int[fii];
 	for (int ii = 0; ii < dii; ii++) 
 	{
-		tmp1[ii] = diff[ii];
-		tmp2[ii] = diff[dii-ii-1];
+		diff[ii] = differences[ii];
+		ffid[ii] = differences[dii-ii-1];
 	}
-	diff = tmp1; ffid = tmp2;
-	dii--; fii = dii;
+	// to main or diffid
 }
 
-void Password::makediff()
+int* Password::makediff()
 {
+	// from Password constructor
 	int ki = 0, hi = 0, temp = -1;
+	int* differences = new int[klen];
 	while (ki < klen)
 	{
 		if (key[ki] == hint[hi])
@@ -83,7 +87,7 @@ void Password::makediff()
 		{
 			if (temp != -1) 
 			{
-				diff[dii] = (ki-hi) - temp;
+				differences[dii] = (ki-hi) - temp;
 				dii++;
 			}
 			temp = ki - hi;
@@ -95,25 +99,44 @@ void Password::makediff()
 			else ki++;
 		}
 	}
-	resize();
+	return differences;
 }
 
 Password diffid(Password pass) // compares diff and ffid
 {
+	// from main or diffid
+	cout << "Diff:: ";
+	for (int ii = 0; ii < pass.dii; ii++) 
+	{
+		printf("%i ", pass.diff[ii]);
+	}
+	cout << endl << "FFid:: ";
+	for (int ii = 0; ii < pass.fii; ii++) 
+	{
+		printf("%i ", pass.ffid[ii]);
+	}
+	cout << endl;
+	printf("(%i, %i)\n", pass.diff[pass.dii-1], pass.ffid[pass.fii-1]);
 	if (pass.diff[pass.dii-1] == pass.ffid[pass.fii-1])
 	{
-		pass.code = to_string(pass.diff[pass.dii]) + " " + pass.code;
-		if (pass.dii != 0 && pass.fii != 0) 
+		printf("Code:: adding %s to %s\n\n", to_string(pass.diff[pass.dii-1]).c_str(), pass.code.c_str());
+		pass.code = to_string(pass.diff[pass.dii-1]) + " " + pass.code;
+		if (pass.dii-1 != 0 && pass.fii-1 != 0) 
 		{
 			pass.dii--; pass.fii--;
 			pass.codelen += 1 + diffid(pass).codelen;
 		}
-		else pass.codelen += 1;
+		else 
+		{
+			pass.codelen += 1;
+			cout << endl << "Hit Base with code = " << pass.code << endl;
+		}
+		cout << endl << "Returning " << pass.code << endl;
 		return pass;
 	}
-	else
+	else // pass.diff[pass.dii] != pass.ffid[pass.fii]
 	{
-		if (pass.dii != 0 && pass.fii != 0)
+		if (pass.dii-1 != 0 && pass.fii-1 != 0)
 		{
 			Password pass1 = Password(pass), pass2 = Password(pass);
 			pass1.dii--; pass2.fii--;
@@ -121,8 +144,10 @@ Password diffid(Password pass) // compares diff and ffid
 			pass.code = (pass1.codelen > pass2.codelen ? pass1.code : pass2.code) + " " + pass.code;
 			pass.codelen += (pass1.codelen > pass2.codelen ? pass1.codelen : pass2.codelen);
 		}
+		cout << endl << "Returning " << pass.code << endl;
 		return pass;
 	}
+	// to main
 }
 
 int main()
@@ -131,7 +156,6 @@ int main()
 	getline(cin, key);
 	getline(cin, hint);
 	Password result = Password(key, hint);
-	diffid(result);
-	cout << result.code;
+	cout << diffid(result).code;
 	return 0;
 }
